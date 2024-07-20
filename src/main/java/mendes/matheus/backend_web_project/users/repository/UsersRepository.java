cd914601e4423febfa6fd272a2ad6bd283741988
Matheus Mendes <msmmendes211@gmail.com>
2024-07-20 20:10:45 -0300

    feat: atualiza a classe UserRepository

--- src/main/java/mendes/matheus/backend_web_project/users/repository/UsersRepository.java
+++ src/main/java/mendes/matheus/backend_web_project/users/repository/UsersRepository.java
@@ -3,5 +3,13 @@ package mendes.matheus.backend_web_project.users.repository;
 import mendes.matheus.backend_web_project.users.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users, Long> {
+
+    Optional<Users> findByEmail(String email);
+
+    Optional<Users> findByUsername(String email);
+
+    Optional<Users> findUsersById(Long userId);
 }
