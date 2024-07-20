5ea7649e730a4e8c0671db6dd5b60286fc7538c0
Matheus Mendes <msmmendes211@gmail.com>
2024-07-20 20:11:52 -0300

    feat: atualiza a migration create-table-users

--- src/main/resources/db/migration/V1__create-table-users.sql
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
     id VARCHAR(255) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
