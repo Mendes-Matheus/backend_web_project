604feeef951ac8dcb1aee79657b17844a99fa1ce
Matheus Mendes <msmmendes211@gmail.com>
2024-07-20 20:12:31 -0300

    feat: atualiza a classe UsersRequestDTO

--- src/main/java/mendes/matheus/backend_web_project/users/dto/UsersRequestDTO.java
+++ src/main/java/mendes/matheus/backend_web_project/users/dto/UsersRequestDTO.java
@@ -1,4 +1,8 @@
 package mendes.matheus.backend_web_project.users.dto;
 
-public record UsersRequestDTO(String name, String email, String password) {
-}
+public record UsersRequestDTO(
+    String name,
+    String email,
+    String username,
+    String password
+) {}
