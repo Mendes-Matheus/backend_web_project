cf2620a808deafb93d261f8c2d7cf4701c8d11c6
Matheus Mendes <msmmendes211@gmail.com>
2024-07-21 19:29:32 -0300

    feat: define tamanhos especifícos para alguns campos com a notação @Size

--- src/main/java/mendes/matheus/backend_web_project/users/dto/UsersRequestDTO.java
+++ src/main/java/mendes/matheus/backend_web_project/users/dto/UsersRequestDTO.java
@@ -3,10 +3,12 @@ package mendes.matheus.backend_web_project.users.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UsersRequestDTO(
 
         @JsonProperty(value = "name", required = true)
+        @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
         @NotBlank(message = "Name is required")
         String name,
 
@@ -16,10 +18,12 @@ public record UsersRequestDTO(
         String email,
 
         @JsonProperty(value = "username")
+        @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
         @NotBlank(message = "Username is required")
         String username,
 
         @JsonProperty(value = "password")
         @NotBlank(message = "Password is required")
+        @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
         String password
 ) {}
