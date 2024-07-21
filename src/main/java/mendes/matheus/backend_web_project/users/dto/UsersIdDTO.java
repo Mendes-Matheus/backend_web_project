c743b1ef8a3a0998a76dbb201731703d3b29524c
Matheus Mendes <msmmendes211@gmail.com>
2024-07-21 12:41:00 -0300

    feat: atualiza UsersRequestDTO e UsersIdDTO

--- src/main/java/mendes/matheus/backend_web_project/users/dto/UsersIdDTO.java
+++ src/main/java/mendes/matheus/backend_web_project/users/dto/UsersIdDTO.java
@@ -1,4 +1,8 @@
 package mendes.matheus.backend_web_project.users.dto;
 
-public record UsersIdDTO(Long userId) {
-}
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record UsersIdDTO(
+    @JsonProperty(value = "userId")
+    Long userId
+) {}
--- src/main/java/mendes/matheus/backend_web_project/users/dto/UsersRequestDTO.java
+++ src/main/java/mendes/matheus/backend_web_project/users/dto/UsersRequestDTO.java
@@ -1,8 +1,25 @@
 package mendes.matheus.backend_web_project.users.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record UsersRequestDTO(
-    String name,
-    String email,
-    String username,
-    String password
+
+        @JsonProperty(value = "name", required = true)
+        @NotBlank(message = "Name is required")
+        String name,
+
+        @JsonProperty(value = "email")
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email should be valid")
+        String email,
+
+        @JsonProperty(value = "username")
+        @NotBlank(message = "Username is required")
+        String username,
+
+        @JsonProperty(value = "password")
+        @NotBlank(message = "Password is required")
+        String password
 ) {}
