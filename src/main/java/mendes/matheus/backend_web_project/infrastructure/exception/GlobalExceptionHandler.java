c73f5fa3ecd6e2a90a65bdd951f81fca03894687
Matheus Mendes <msmmendes211@gmail.com>
2024-07-26 19:36:11 -0300

    fix: corrige GlobalExceptionHandler

--- src/main/java/mendes/matheus/backend_web_project/infrastructure/exception/GlobalExceptionHandler.java
+++ src/main/java/mendes/matheus/backend_web_project/infrastructure/exception/GlobalExceptionHandler.java
@@ -8,14 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     /**
