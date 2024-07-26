435c091891af043ebdaf95aeebb8dd9f9771a800
Matheus Mendes <msmmendes211@gmail.com>
2024-07-26 19:35:21 -0300

    fix: corrige PersistenceEntity

--- src/main/java/mendes/matheus/backend_web_project/infrastructure/model/PersistenceEntity.java
+++ src/main/java/mendes/matheus/backend_web_project/infrastructure/model/PersistenceEntity.java
@@ -3,9 +3,11 @@ package mendes.matheus.backend_web_project.infrastructure.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @MappedSuperclass
-public class PersistenceEntity {
+public class PersistenceEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
