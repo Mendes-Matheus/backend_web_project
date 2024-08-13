a9f79c0ddc92518237007f4169569bb55199a309
Matheus Mendes <msmmendes211@gmail.com>
2024-08-12 21:41:04 -0300

    feat: adiciona os métodos getUserById e getUserByEmail no service

--- src/main/java/mendes/matheus/backend_web_project/users/service/UsersService.java
+++ src/main/java/mendes/matheus/backend_web_project/users/service/UsersService.java
@@ -2,13 +2,10 @@ package mendes.matheus.backend_web_project.users.service;
 
 import lombok.RequiredArgsConstructor;
 import mendes.matheus.backend_web_project.infrastructure.mapper.ObjectMapperUtil;
-import mendes.matheus.backend_web_project.users.dto.UsersSimpleResponseDTO;
-import mendes.matheus.backend_web_project.users.dto.UsersSummaryResponseDTO;
-import mendes.matheus.backend_web_project.users.dto.UsersUpdateDTO;
+import mendes.matheus.backend_web_project.users.dto.*;
 import mendes.matheus.backend_web_project.users.exceptions.UserAlreadyExistsException;
 import mendes.matheus.backend_web_project.users.exceptions.UserNotFoundException;
 import mendes.matheus.backend_web_project.users.model.Users;
-import mendes.matheus.backend_web_project.users.dto.UsersRequestDTO;
 import mendes.matheus.backend_web_project.users.repository.UsersRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -58,15 +55,42 @@ public class UsersService {
         }
     }
 
-
-    public Users getUserById(Long userId){
-        return this.usersRepository.findUsersById(userId).orElseThrow(()
-                -> new UserNotFoundException("User not found with ID: " + userId));
+    public Users getUserEntityById(Long userId) {
+        return this.usersRepository.findById(userId).orElseThrow(() ->
+                new UserNotFoundException("User not found with ID: " + userId));
     }
 
-    public Users getUserByEmail(String email){
-        return this.usersRepository.findByEmail(email).orElseThrow(()
-                -> new UserNotFoundException("User not found with email: " + email));
+
+
+    public UsersSimpleResponseDTO getUserById(Long userId){
+        Users user = this.usersRepository.findById(userId).orElseThrow(() ->
+                new UserNotFoundException("User not found with ID: " + userId));
+        return new UsersSimpleResponseDTO(user.getUsername());
+    }
+
+
+    /**
+     * Método responsável por buscar um usuário pelo username
+     *
+     * @param username
+     * @return retorna um objeto UsersSimpleResponseDTO com o username do usuário encontrado
+     */
+    public UsersSimpleResponseDTO getUserByUsername(String username) {
+        Users user = usersRepository.findByUsername(username).orElseThrow(() ->
+                new UserNotFoundException("User not found with username: " + username));
+        return new UsersSimpleResponseDTO(user.getUsername());
+    }
+
+    /**
+     * Método responsável por buscar um usuário pelo email
+     *
+     * @param email
+     * @return retorna um objeto UsersSimpleResponseDTO com o username do usuário encontrado
+     */
+    public UsersSimpleResponseDTO getUserByEmail(String email) {
+        Users user = usersRepository.findByEmail(email).orElseThrow(() ->
+                new UserNotFoundException("User not found with email: " + email));
+        return new UsersSimpleResponseDTO(user.getUsername());
     }
 
     public List<UsersSummaryResponseDTO> getAllUsersDTO() {
@@ -82,7 +106,7 @@ public class UsersService {
      * retorna um objeto UsersSimpleResponseDTO com o username do usuário atualizado
      */
     public UsersSimpleResponseDTO updateUser(Long userId, @RequestBody @Validated UsersUpdateDTO usersUpdateDTO) {
-        Users existingUser = getUserById(userId);
+        Users existingUser = getUserEntityById(userId);
 
         // Atualiza os campos do usuário
         existingUser.setName(usersUpdateDTO.name());
@@ -101,7 +125,7 @@ public class UsersService {
      * @param userId
      */
     public void deleteUser(Long userId) {
-        Users existingUser = getUserById(userId);
+        Users existingUser = getUserEntityById(userId);
         this.usersRepository.delete(existingUser);
     }
 
