db0bc7af4e407517c9dbd31de6da427603cc77b3
Matheus Mendes <msmmendes211@gmail.com>
2024-08-23 14:16:47 -0300

    refactor: atualização para consumir a api com web client no lugar do rest template

--- src/main/java/mendes/matheus/backend_web_project/BackendWebProjectApplication.java
+++ src/main/java/mendes/matheus/backend_web_project/BackendWebProjectApplication.java
@@ -1,10 +1,27 @@
 package mendes.matheus.backend_web_project;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 @SpringBootApplication
 public class BackendWebProjectApplication {
+
+	@Value("${viacep_api_url}")
+	private String viaCepApiUrl;
+
+	@Bean
+	public WebClient webClient(WebClient.Builder builder) {
+		return builder
+				.baseUrl(viaCepApiUrl)
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendWebProjectApplication.class, args);
--- src/main/java/mendes/matheus/backend_web_project/users/service/ViaCepService.java
+++ src/main/java/mendes/matheus/backend_web_project/users/service/ViaCepService.java
@@ -2,17 +2,26 @@ package mendes.matheus.backend_web_project.users.service;
 
 import lombok.RequiredArgsConstructor;
 import mendes.matheus.backend_web_project.users.dto.AddressDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
 public class ViaCepService {
 
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
+    private static final Logger log = LoggerFactory.getLogger(UsersService.class);
 
-    public AddressDTO getAddressByCep(String cep) {
-        String url = "https://viacep.com.br/ws/" + cep + "/json/";
-        return restTemplate.getForObject(url, AddressDTO.class);
+    public Mono<AddressDTO> getAddressByCep(String cep) {
+        return webClient
+                .get()
+                .uri(cep + "/json/")
+                .retrieve()
+                .bodyToMono(AddressDTO.class)
+                .doOnNext(address -> log.info("Fetched address: {}", address))
+                .doOnError(error -> log.error("Error fetching address", error));
     }
 }
